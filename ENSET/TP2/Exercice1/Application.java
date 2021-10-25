25788a53bb5d848606d42abd2f6fb26722499dd2
ELYOUSFI Mohamed <73071155+ElyousfiMohamed@users.noreply.github.com>
2021-10-25 20:20:27 +0100

    Add files via upload

---
+++ ENSET/TP2/Exercice1/Application.java
@@ -0,0 +1,15 @@
+package TP2.Exercice1;
+
+public class Application {
+    public static void main(String[] args) {
+        Adherent adh1 = new Adherent("ELYOUSFI", "Mohamed", "mohamed5elyousfi@gmail.com", "+212622222222", 21, 1258);
+        System.out.println("affichage des informations d'Adherent");
+        adh1.afficher();
+        
+
+        Auteur aut1 = new Auteur("TAIFAOUI", "Abdellah", "taifaoui7@gmail.com", "+212688888888", 21, 5);
+        Livre livre1 = new Livre(9586, "of mice and man", aut1);
+        System.out.println("\n\n\naffichage des informations du Livre");
+        livre1.afficher();
+    }
+}
