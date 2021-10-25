4524f9d9381b3a74e3a431a00789239b9fa80764
ELYOUSFI Mohamed <73071155+ElyousfiMohamed@users.noreply.github.com>
2021-10-25 20:55:08 +0100

    Add files via upload

---
+++ ENSET/TP2/Exercice2/Reponse.java
@@ -0,0 +1,16 @@
+package TP2.Exercice2;
+
+public class Reponse {
+    private String titre;
+    private boolean correcte;
+
+    public Reponse(String titre, boolean correcte) {
+        this.titre = titre;
+        this.correcte = correcte;
+    }
+
+    @Override
+    public String toString() {
+        return "titre : " + titre + " / " + correcte;
+    }
+}
